5f1992651002800d40901d7348b4189f80556391
high1 <radomir.simic@gmail.com>
2020-05-13 16:13:37 +0200

    Build & split chunks
    - add build script and split chunks in that case
    - update packages

--- webpack.config.ts
+++ webpack.config.ts
@@ -68,6 +68,25 @@ const config = (_env: unknown, { mode = 'none' }: Configuration): Configuration 
     minimizer: [
       ...(mode === 'development' ? [] : [new TerserWebackPlugin(), new OptimizeCSSAssetsPlugin()]),
     ],
+    runtimeChunk: mode === 'development' ? undefined : 'single',
+    splitChunks:
+      mode === 'development'
+        ? false
+        : {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+              vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name(module): string {
+                  return `${module.context
+                    .match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                    .replace('@', '')}`;
+                },
+              },
+            },
+          },
   },
 });
 
